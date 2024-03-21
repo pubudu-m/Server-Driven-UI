8c962026923273f785e1bbcbfaeb11efb9dbf141
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2024-03-21 08:29:41 +0530

    Initial Commit

---
+++ SDUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDUI
+//
+//  Created by Pubudu Mihiranga on 2024-03-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDUI/SDUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDUIApp.swift
+//  SDUI
+//
+//  Created by Pubudu Mihiranga on 2024-03-21.
+//
+
+import SwiftUI
+
+@main
+struct SDUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
